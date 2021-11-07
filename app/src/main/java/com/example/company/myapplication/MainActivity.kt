504a87dedf4f403d2e41015063e1bfc6640fb567
golevArtemOhta <golevartem1995@gmail.com>
2021-11-07 19:16:19 +0300

    nit_spinner_dynamic_list

---
+++ app/src/main/java/com/example/company/myapplication/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.company.myapplication
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.NumberFormatException
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val data = MutableList(0, { x -> "$x" })
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, data)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {
+
+                try {
+                    data.clear()
+                    for (i in 1..editText.text.toString().toInt()){
+                        data.add(i.toString())
+                    }
+
+                } catch (e: NumberFormatException){
+                    data.clear()
+                }
+
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+        })
+
+    }
+}
